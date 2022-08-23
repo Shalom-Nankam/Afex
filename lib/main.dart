1a1221a458920f78e617b293d478f200bff120c3
Shalom-Nankam <shalomabrokwah@gmail.com>
2022-08-23 12:52:52 +0100

    Made the app a little responsive using the responsive framework package

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,27 @@ import 'package:farm_location/MVC/Controllers/location_controller.dart';
 import 'package:farm_location/cubit/cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
-void main() => runApp(
-  BlocProvider<LocationCubit>(
-    create: (context) => LocationCubit(),
-    child: MyApp(),
-  )
-);
+void main() => runApp(BlocProvider<LocationCubit>(
+      create: (context) => LocationCubit(),
+      child: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      builder: (context, Widget) => ResponsiveWrapper.builder(
+          ClampingScrollWrapper.builder(context, Widget!),
+          breakpoints: const [
+            ResponsiveBreakpoint.resize(350, name: MOBILE),
+            ResponsiveBreakpoint.resize(800, name: DESKTOP),
+            ResponsiveBreakpoint.autoScale(600, name: TABLET),
+            ResponsiveBreakpoint.autoScale(1700, name: 'XL'),
+          ]),
       title: 'Material App',
-      home: LocationScreen(),
+      home: const LocationScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
